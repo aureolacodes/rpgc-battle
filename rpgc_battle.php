83a7733cbc03d6acfd76777d4d73b29619d7b247
Christian Hanne <mail@christianhanne.de>
2014-01-15 21:59:25 +0100

    Reduced overall line length by using shorts for several variables.

--- rpgc_battle.php
+++ rpgc_battle.php
@@ -61,74 +61,79 @@ class RpgBattle {
   /**
    * Solves a battle between two opponents.
    *
-   * @param $attacker
-   * @param $defender
+   * @param array $att
+   *   An array that represents the attacker.
+   *   Attacker gets to strike first in a battle.
+   * @param array $def
+   *   An array that represents the defender.
    * @return mixed
+   *   Returns either an array or FALSE.
    */
-  public static solve($attacker = array(), $defender = array()) {
+  public static solve($att = array(), $def = array()) {
     $return = FALSE;
-    if (self::validateOpponent($attacker) && self::validateOpponent($defender)) {
+    if (self::validateOpponent($att) && self::validateOpponent($def)) {
       $rounds = array();
 
       // calculate chance to hit (cth) for attacker and defender, this will be a percent rate
-      $attacker['cth'] = (($attacker['attack'] - $defender['defense']) / $attacker['attack']) * 100;
-      $defender['cth'] = (($defender['attack'] - $attacker['defense']) / $defender['attack']) * 100;
+      $att['cth'] = (($att['attack'] - $def['defense']) / $att['attack']) * 100;
+      $def['cth'] = (($def['attack'] - $att['defense']) / $def['attack']) * 100;
 
       // no negative values allowed, otherwise this fighting system won't work
-      if ($attacker['cth'] < 0) $attacker['cth'] = 0;
-      if ($defender['cth'] < 0) $defender['cth'] = 0;
+      $att['cth'] = $att['cth'] > 0 ? $att['cth'] : 0;
+      $def['cth'] = $def['cth'] > 0 ? $def['cth'] : 0;
 
       // if both cth equal zero, the fighters won't be able to hit each other, so let's just return
-      if ($attacker['cth'] == 0 && $defender['cth'] == 0) {
+      if ($att['cth'] == 0 && $def['cth'] == 0) {
         $result = self::RPGC_BATTLE_DRAW;
       }
       else {
         // calculate damage attacker and defender will be able to deal to each other
-        $attacker['dmg_rate'] = (($attacker['damage'] - $defender['armor']) / $attacker['damage']) * 100;
-        $defender['dmg_rate'] = (($defender['damage'] - $attacker['armor']) / $defender['damage']) * 100;
+        $att['dmg_rate'] = (($att['damage'] - $def['armor']) / $att['damage']) * 100;
+        $def['dmg_rate'] = (($def['damage'] - $att['armor']) / $def['damage']) * 100;
 
         // same as above
-        if ($attacker['dmg_rate'] < 0) $attacker['dmg_rate'] = 0;
-        if ($defender['dmg_rate'] < 0) $defender['dmg_rate'] = 0;
+        $att['dmg_rate'] = $att['dmg_rate'] > 0 ? $att['dmg_rate'] : 0;
+        $def['dmg_rate'] = $def['dmg_rate'] > 0 ? $def['dmg_rate'] : 0;
 
         // if both fighters can't deal any damage, it might be better to just break here
-        if ($attacker['dmg_rate'] == 0 && $defender['dmg_rate'] == 0) {
+        if ($att['dmg_rate'] == 0 && $def['dmg_rate'] == 0) {
           $result = RPGC_BATTLE_DRAW;
         }
         else {
           // to determin when the fight is over, both fighters get a fake health bar
-          $attacker['health'] = $defender['health'] = self::RPGC_BATTLE_INIT_HEALTH;
+          $att['health'] = isset($att['health']) ? $att['health'] : self::RPGC_BATTLE_INIT_HEALTH;
+          $def['health'] = isset($def['health']) ? $def['health'] : self::RPGC_BATTLE_INIT_HEALTH;
 
           // now both fighters are prepared, let's begin the fighting
-          while ($attacker['health'] > 0 && $defender['health'] > 0) {
+          while ($att['health'] > 0 && $def['health'] > 0) {
             $round = array();
 
             // the attacker started the battle, so he begins
             $round['attacker_roll'] = rand(0, 100);
 
             // if the throw is smaller then the chance to hit, he hits the defender
-            if ($round['attacker_roll'] <= $attacker['cth']) {
-              $round['attacker_damage'] = rand(0, $attacker['dmg_rate']);
-              $defender['health'] -= $round['attacker_damage'];
+            if ($round['attacker_roll'] <= $att['cth']) {
+              $round['attacker_damage'] = rand(0, $att['dmg_rate']);
+              $def['health'] -= $round['attacker_damage'];
             }
 
             // if the defender is still alive, it's his turn now
-            if ($attacker['dmg_rate'] > 0 && $defender['health'] > 0) {
+            if ($att['dmg_rate'] > 0 && $def['health'] > 0) {
               $round['defender_roll'] = rand(0, 100);
-              if ($round['defender_roll'] <= $defender['cth']) {
-                $round['defender_damage'] = rand(0, $defender['dmg_rate']);
-                $attacker['health'] -= $round['defender_damage'];
+              if ($round['defender_roll'] <= $def['cth']) {
+                $round['defender_damage'] = rand(0, $def['dmg_rate']);
+                $att['health'] -= $round['defender_damage'];
               }
             }
 
-            $round['attacker_health'] = $attacker['health'];
-            $round['defender_health'] = $defender['health'];
+            $round['attacker_health'] = $att['health'];
+            $round['defender_health'] = $def['health'];
 
             array_push($rounds, $round);
           }
 
           // check who won this fight
-          if ($attacker['health'] <= 0) {
+          if ($att['health'] <= 0) {
             $result = self::RPGC_BATTLE_DEF_WINS;
           }
           else {
@@ -139,8 +144,8 @@ class RpgBattle {
 
       // Gather information for return.
       $return = array(
-        'attacker' => $attacker,
-        'defender' => $defender,
+        'attacker' => $att,
+        'defender' => $def,
         'result'   => $result,
         'rounds'   => $rounds,
       );
